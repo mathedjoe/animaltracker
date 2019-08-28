d951d2e8cf5288c3ea6373b3dfe381a57cea75d2
twotailstats <twotailstatistics@gmail.com>
2019-08-27 22:37:02 -0600

    refactor javascript to address note

--- R/app_ui.R
+++ R/app_ui.R
@@ -9,10 +9,19 @@
 #'
 
 app_ui <- function(){
+  
+  jsCode <- 'shinyjs.removePolygon = function() {
+    var event = document.createEvent("Event");
+    event.initEvent("click", true, true);
+    var trashButton = document.getElementsByClassName("leaflet-draw-edit-remove");
+    !trashButton[0].dispatchEvent(event);
+    var clearButton = document.querySelector(\'[title="Clear all layers"]\');
+    !clearButton.dispatchEvent(event);
+  }'
     navbarPage(theme = shinytheme("yeti"),
              # shinythemes::themeSelector(),  # <--- run this to choose a style theme
              header = div(useShinyjs(),
-                          extendShinyjs(script = "js/removepolygon.js")),
+                          extendShinyjs(text = jsCode)),
              title = "Animal Tracker",
              
              ## DATA PANEL
