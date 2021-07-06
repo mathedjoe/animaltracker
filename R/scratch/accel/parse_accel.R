72a04aaf773706ed94de9d76dd030ff0ba603b11
mathedjoe <joechampion@boisestate.edu>
2021-07-06 15:01:26 -0600

    initial parsing of behavior data

--- R/scratch/accel/parse_accel.R
+++ R/scratch/accel/parse_accel.R
@@ -3,11 +3,12 @@ library(lubridate)
 library(sp)
 library(zoo)
   
+### READ INTEGRATED ACCEL/GPS DATA FROM STANDARDIZED FORMAT
 # metadata is always 10 lines
-sat_line <- colnames(read_csv("test_data/DATA-001.CSV", skip = 8, n_max = 0)) # jump to the 9th line
+sat_line <- colnames(read_csv("test_data/test_accel/DATA-001.CSV", skip = 8, n_max = 0)) # jump to the 9th line
 num_sat <- as.numeric(sat_line[length(sat_line)]) # get number of satellites
 # there is always 1 extra metadata line after satellite list
-accel_raw <- read_csv("test_data/DATA-001.CSV", skip = 10 + num_sat + 1) %>% 
+accel_raw <- read_csv("test_data/test_accel/DATA-001.CSV", skip = 10 + num_sat + 1) %>% 
   dplyr::rename(Time = ";Time") %>% 
   dplyr::mutate(Time = as.numeric(Time),
                 real_time = strftime(lubridate::as_datetime(Time, tz = "UTC"), "%Y-%m-%d %H:%M:%OS3", tz = "UTC")) %>% 
@@ -30,3 +31,43 @@ accel_reformat <- accel_raw %>%
                 Course = calc_bearing(dplyr::lag(Latitude, 1, default = first(Latitude)), dplyr::lag(Longitude, 1, default = first(Longitude)), Latitude, Longitude),
                 Distance = geosphere::distGeo(cbind(Longitude, Latitude), 
                                               cbind(dplyr::lag(Longitude,1,default=first(Longitude)), dplyr::lag(Latitude,1,default=first(Latitude) ))))
+
+### READ TIMESTAMPED BEHAVIOR DATA FROM XLSM SPREADSHEET
+test_behavior_file <- list.files("test_data/test_accel", pattern = ".xlsm", full.names=TRUE)[1]
+
+behavior_sheets <- readxl::excel_sheets(test_behavior_file)
+
+custom_name_repair <- function(name_text){ifelse(name_text =="", "XX", tolower(gsub("\\ ", "_", name_text)))}
+
+behavior_raw_1sheet <-   readxl::read_excel(test_behavior_file, 
+                                            sheet = behavior_sheets[3],
+                                            range = readxl::cell_cols(c(1:31)),
+                                            col_types = "text", 
+                                            .name_repair = custom_name_repair) %>% 
+  dplyr::select(-XX) %>% 
+  type.convert() 
+
+behavior_raw_1sheet 
+
+behavior_raw <- lapply(behavior_sheets, function(sheet_name){
+  readxl::read_excel(test_behavior_file, 
+                     sheet = sheet_name,
+                     range = readxl::cell_cols(c(1:31)),
+                     col_types = "text", 
+                     .name_repair = custom_name_repair) %>% 
+    dplyr::select(-XX) %>%
+    # drop any rows that are all NA (except cowid)
+    filter(if_any(setdiff(everything(),one_of("cowid")) , ~ !is.na(.)))
+  }) %>% 
+  dplyr::bind_rows() %>% 
+  type_convert()
+
+
+behavior_long <- behavior_raw %>% 
+  pivot_longer(drinking:walking, names_to = "behavior") %>% 
+  filter(!is.na(value)) %>% 
+  select(-value) %>% 
+  pivot_longer(contains("elapsed"), values_to = "time_elapsed") %>% 
+  select(-name)
+
+
